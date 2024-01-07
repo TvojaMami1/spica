e0bfd77f68b1366857a3eebbd22e18ba1a81d400
TvojaMami <nejkojager@gmail.com>
2024-01-07 18:51:19 +0100

    Search users working!

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,30 +2,43 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { UsersService } from '../users.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   //templateUrl: './app.component.html',
   styleUrl: './users.component.css',
   template: `
   <h2>Users</h2>
   @if (showUsers) {
-    <form>
-      <input type="text">
-      <button>Submit</button>
+    <form [formGroup]="userForm" (ngSubmit)="searchUser()">
+      <input type="text" formControlName="fullName">
+      <button type="submit">Search</button>
     </form>
+    @if (userFormView) {
+      <button (click)="showAll()">Clear</button>
+    }
     <table class="niceTable">
       <tr>
         <th>First Name</th>
         <th>Last Name</th>
       </tr>
-      @for (item of usersData; track usersData.id) {
-        <tr>
-          <td>{{item.FirstName}}</td>
-          <td>{{item.LastName}}</td>
-        </tr>
+      @if (userFormView) {
+        @for (item of searchUserData; track usersData.id) {
+          <tr>
+            <td>{{item.FirstName}}</td>
+            <td>{{item.LastName}}</td>
+          </tr>
+        }
+      } @else {
+        @for (item of usersData; track usersData.id) {
+          <tr>
+            <td>{{item.FirstName}}</td>
+            <td>{{item.LastName}}</td>
+          </tr>
+        }
       }
     </table>
   } @else {
@@ -36,6 +49,11 @@ import { UsersService } from '../users.service';
   providers: [UsersService]
 })
 export class UsersComponent {
+  userForm = new FormGroup({
+    fullName: new FormControl(""),
+  })
+  userFormView = false;
+
   title = 'Users';
   token = window.localStorage.getItem("token");
   tokenObj = JSON.parse(this.token || "");
@@ -43,6 +61,7 @@ export class UsersComponent {
   showUsers = true;
   keys: any = [];
   usersData: any;
+  searchUserData: any;
 
   constructor(private userService: UsersService) {
     this.users = this.userService.getUsers().subscribe(
@@ -69,5 +88,26 @@ export class UsersComponent {
     this.usersData = dataArr;
     this.keys = Object.keys(dataArr);
     console.log(this.keys);
+  }
+
+  searchUser() {
+    console.log("filtering");
+    this.userFormView = true;
+    this.searchUserData = [];
+    for (let user of this.usersData) {
+      //if (this.userForm.value.fullName || "" in user.FullName) {
+      //  this.searchUserData.push({"FirstName": user.FirstName, "LastName": user.LastName});
+      //}
+      //console.log(user.FullName);
+      if ((user.FullName as string).includes(this.userForm.value.fullName || "")) {
+        this.searchUserData.push(user)
+      }
+      //console.log((user.FullName as string).includes(this.userForm.value.fullName || ""))
+    }
+  }
+
+  showAll() {
+    this.userFormView = false;
+    this.userForm.get("fullName")?.reset();
   }
 }
